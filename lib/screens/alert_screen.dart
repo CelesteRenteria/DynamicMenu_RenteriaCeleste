966c8a706fe22ad2cf492ad5b6e89cc5c9d30189
CelesteRenteria <celeste.renteria.is@unipolidgo.edu.mx>
2022-06-24 08:37:05 -0500

    alert,animation and cards

--- lib/screens/alert_screen.dart
+++ lib/screens/alert_screen.dart
@@ -7,10 +7,35 @@ class AlertScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("AlertScreen")),
-      body: const Center(
-        child: Text('AlertScreen'),
-        //Agregarle in AlertWidget
+      body: const Center(child: AlertW()),
+    );
+  }
+}
+
+class AlertW extends StatelessWidget {
+  const AlertW({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      onPressed: () => showDialog<String>(
+        context: context,
+        builder: (BuildContext context) => AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: const Text('AlertDialog description'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text('Cancel'),
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context, 'OK'),
+              child: const Text('OK'),
+            ),
+          ],
+        ),
       ),
+      child: const Text('Show Dialog'),
     );
   }
 }
--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -6,11 +6,76 @@ class CardScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Card Screen"),
+        appBar: AppBar(
+          title: const Text("Card Screen"),
+        ),
+        body: Column(
+          children: const <Widget>[
+            Spacer(),
+            ElevatedCardExample(),
+            FilledCardExample(),
+            OutlinedCardExample(),
+            Spacer(),
+          ],
+        ));
+  }
+}
+
+class ElevatedCardExample extends StatelessWidget {
+  const ElevatedCardExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Card(
+        child: SizedBox(
+          width: 300,
+          height: 100,
+          child: Center(child: Text('Elevated Card')),
+        ),
       ),
-      body: const Center(
-        child: Text('CardScreen'),
+    );
+  }
+}
+
+class FilledCardExample extends StatelessWidget {
+  const FilledCardExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 4,
+        color: Colors.amber,
+        child: const SizedBox(
+          width: 300,
+          height: 100,
+          child: Center(child: Text('Filled Card')),
+        ),
+      ),
+    );
+  }
+}
+
+class OutlinedCardExample extends StatelessWidget {
+  const OutlinedCardExample({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Card(
+        elevation: 0,
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.outline,
+          ),
+          borderRadius: const BorderRadius.all(Radius.circular(12)),
+        ),
+        child: const SizedBox(
+          width: 300,
+          height: 100,
+          child: Center(child: Text('Outlined Card')),
+        ),
       ),
     );
   }
